805c02ca1063984bfa8ec1b63834e8e7a2388b18
Muhamad Luthfie La Roeha <luthfielaroeha@gmail.com>
2017-02-15 18:48:18 +0000

    GraphQL actionModel (list all action)

--- colsys-backend/graphql/colsys/actionModel.go
+++ colsys-backend/graphql/colsys/actionModel.go
@@ -6,60 +6,40 @@ import (
 )
 
 type action struct {
-	ID        graphql.ID
-	Name      string
-	CallbackFn string
-}
-
-var actions = []*action{
-	{
-		ID:        "1000",
-		Name:      "Action 1",
-		CallbackFn:"Callback function action 1",
-	},
-	{
-		ID:        "1001",
-		Name:      "Action 2",
-		CallbackFn:"Callback function action 2",
-	},
-	{
-		ID:        "1002",
-		Name:      "Action 3",
-		CallbackFn:"Callback function action 3",
-	},
-	{
-		ID:        "1003",
-		Name:      "Action 4",
-		CallbackFn:"Callback function action 4",
-	},
-	{
-		ID:        "1004",
-		Name:      "Action 5",
-		CallbackFn:"Callback function action 5",
-	},
-}
-
-var actionData = make(map[graphql.ID]*action)
-
-func init() {
-	for _, h := range actions {
-		actionData[h.ID] = h
-	}
+	id        graphql.ID
+	name      string
+	callbackFn string
 }
 
 func (r *Resolver) Actions() *[]*actionResolver {
-	var a []*actionResolver
-	for _, action:= range actions {
-		a = append(a, &actionResolver{action})
+	rows, err := conn.Query("SELECT id, name, callbackFn FROM action WHERE isDeleted=false")
+	if err != nil {
+		log.Fatal(err)
 	}
-	return &a
+
+	defer rows.Close()
+
+	var actions []*actionResolver
+	for rows.Next() {
+		var a action
+		err = rows.Scan(&a.id, &a.name, &a.callbackFn)
+		if err != nil {
+			log.Fatal(err)
+		}
+		actions = append(actions, &actionResolver{a})
+	}
+
+	return &actions
 }
 
 func (r *Resolver) Action(args *struct{ ID graphql.ID }) *actionResolver {
-	if a := actionData[args.ID]; a != nil {
-		return &actionResolver{a}
+	var a action
+	err := conn.QueryRow("SELECT id, name, callbackFn FROM action WHERE id=$1 AND isDeleted=false", args.ID).Scan(&a.id, &a.name, &a.description)
+	if err != nil {
+		log.Fatal(err)
 	}
-	return nil
+
+	return &actionResolver{a}
 }
 
 type actionResolver struct {
@@ -78,34 +58,18 @@ func (a *actionResolver) CallbackFn() string {
 	return a.a.CallbackFn
 }
 
-func listTasks() error {
-	rows, _ := conn.Query("select * from tasks")
-
-	for rows.Next() {
-		var id int32
-		var description string
-		err := rows.Scan(&id, &description)
-		if err != nil {
-			return err
-		}
-		fmt.Printf("%d. %s\n", id, description)
-	}
-
-	return rows.Err()
-}
-
-func addTask(description string) error {
-	_, err := conn.Exec("insert into tasks(description) values($1)", description)
+func addAction(name string, description string) error {
+	_, err := conn.Exec("INSERT INTO action(name, description) VALUES($1, $2)", name, description)
 	return err
 }
 
-func updateTask(itemNum int32, description string) error {
-	_, err := conn.Exec("update tasks set description=$1 where id=$2", description, itemNum)
+func updateAction(id graphql.ID, name string, description string) error {
+	_, err := conn.Exec("UPDATE action SET name=$1, description=$2 WHERE id=$3", name, description, id)
 	return err
 }
 
-func removeTask(itemNum int32) error {
-	_, err := conn.Exec("delete from tasks where id=$1", itemNum)
+func removeAction(id graphql.ID) error {
+	_, err := conn.Exec("UPDATE action SET isDeleted=true WHERE id=$1", id)
 	return err
 }
 
